cf1da2f507197e8da1514381578097040d58aa8b
rayyan234 <84240080+rayyan234@users.noreply.github.com>
2022-02-19 13:14:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(400,400);
+canvas.position(430,170);
+}
+
+function draw(){
+
+}
+
+function takesnapshot(){
+    save('mysnapshot.png');
+}
